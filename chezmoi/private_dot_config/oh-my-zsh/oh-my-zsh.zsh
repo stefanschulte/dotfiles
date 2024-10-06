eca1669b091ee7e9ae80d6304e37558ce996b6ad
Stefan Schulte <github@stefanschulte.dev>
2024-10-06 23:40:57 +0200

    Fix stuttering on pasting text

--- chezmoi/private_dot_config/oh-my-zsh/oh-my-zsh.zsh
+++ chezmoi/private_dot_config/oh-my-zsh/oh-my-zsh.zsh
@@ -12,6 +12,19 @@ source $ZSH/oh-my-zsh.sh
 export ZPLUG_HOME="$HOME/.zplug"
 source $ZPLUG_HOME/init.zsh
 
+# This speeds up pasting w/ autosuggest
+# https://github.com/zsh-users/zsh-autosuggestions/issues/238
+pasteinit() {
+  OLD_SELF_INSERT=${${(s.:.)widgets[self-insert]}[2,3]}
+  zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
+}
+
+pastefinish() {
+  zle -N self-insert $OLD_SELF_INSERT
+}
+zstyle :bracketed-paste-magic paste-init pasteinit
+zstyle :bracketed-paste-magic paste-finish pastefinish
+
 # install powerlevel10k
 zplug "romkatv/powerlevel10k", as:theme, depth:1
 # oh-my-zsh plugins
